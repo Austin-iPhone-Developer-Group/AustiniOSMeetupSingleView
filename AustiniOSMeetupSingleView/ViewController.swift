99fb1cfaf39f46cf7ace44cee94df1a8f10a4ad9
murraysagal <murraysagal@mac.com>
2017-12-05 20:52:59 -0600

    working table view.

--- AustiniOSMeetupSingleView/ViewController.swift
+++ AustiniOSMeetupSingleView/ViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let letters = "abcdefghijklmnopqrstuvwxyz".map {return String($0)}
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return letters.count
     }
-
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        print("cell.textLabel?.text: \(cell.textLabel?.text ?? "")")
+        cell.textLabel?.text = letters[indexPath.row]
+        cell.accessoryType = indexPath.row % 2 == 1 ? .checkmark : .none
+        return cell
+    }
 }
 
